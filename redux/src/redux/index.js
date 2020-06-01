a6fb5521890e5dd5e2a51815f20ae097595e537e
Adela2012 <15757124551@163.com>
2020-06-01 22:15:52 +0800

    self redux

---
+++ redux/src/redux/index.js
@@ -0,0 +1,9 @@
+import createStore from './createStore'
+import applyMiddleware from './applyMiddleware'
+import combineReducers from './combineReducers'
+
+export {
+    applyMiddleware,
+    createStore,
+    combineReducers
+}
