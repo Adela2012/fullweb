4be16e9e4fdb436eabcda2aef4627bec47b91d37
Adela2012 <15757124551@163.com>
2020-12-13 15:15:15 +0800

    add mp

--- mp/cloud/miniprogram/pages/add/add.js
+++ mp/cloud/miniprogram/pages/add/add.js
@@ -1,5 +1,39 @@
 // miniprogram/pages/add/add.js
+let canvas;
+let ctx;
 Page({
+  saveCanvas() {
+    wx.canvasToTempFilePath({
+      canvas: canvas,
+      success:(res)=>{
+        console.log(res);
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+        })
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+    const query = wx.createSelectorQuery()
+    query.select('#myCanvas')
+      .fields({ node: true, size: true })
+      .exec((res) => {
+        canvas = res[0].node
+        ctx = canvas.getContext('2d')
+
+        const dpr = wx.getSystemInfoSync().pixelRatio
+        canvas.width = res[0].width * dpr
+        canvas.height = res[0].height * dpr
+        ctx.scale(dpr, dpr)
+
+        ctx.fillStyle='black'
+        ctx.fillRect(0, 0, 300, 300)
+      })
+  },
 
   /**
    * Page initial data
@@ -15,6 +49,14 @@ Page({
 
   },
 
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
   askFor() {
     wx.cloud.callFunction({
       name: 'add',
@@ -24,21 +66,6 @@ Page({
       }
     })
   },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },›
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
   /**
    * Lifecycle function--Called when page hide
    */
