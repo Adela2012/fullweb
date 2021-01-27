91852e94c2bbb00665159571f0c35ecaaca6a82f
Adela2012 <15757124551@163.com>
2021-01-27 08:55:49 +0800

    fs

--- node/sql/fs.js
+++ node/sql/fs.js
@@ -1,11 +1,49 @@
 const fs = require('fs')
 
 
-function get(val) {
-
+function get(key) {
+    fs.readFile('./db.json', (err, data) => {
+        const json = data ? JSON.parse(data) : {}
+        console.log(json[key]) 
+    })
 }
 
 
-function set (key, val) {
+function set (key, value) {
+    fs.readFile('./db.json', (err, data) => {
+        const json = data ? JSON.parse(data) : {}
+        json[key] = value
 
+        fs.writeFile('./db.json', JSON.stringify(json), err=> {
+            if (err) console.log(err)
+            console.log('write ok')
+
+        })
+    })
 }
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.on('line', function(input){
+    const [op, key, value] = input.split(' ')
+    if (op == 'get') {
+        get(key)
+    }
+    else if (op == 'set') {
+        set(key, value)
+    }
+    else if (op == 'quit') {
+        rl.close()
+    } else {
+        console.log('no operation')
+    }
+})
+
+rl.on('close', function () {
+    console.log('close program')
+    process.exit(0)
+})
